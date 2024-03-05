1672a688f0bcf8e4fd6caacc7f809a7a8262681a
Marat <maratmig@gmail.com>
2024-03-05 16:58:13 +0200

    GitHub pages 3 commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/reflectiz'),
+      dir: require('path').join(__dirname, './coverage/maratmig.github.io'),
       subdir: '.',
       reporters: [
         { type: 'html' },
